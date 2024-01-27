d77057d6365f0766089af2557dffdbbde25f9bed
Alfred Zimo <zimoalfred@gmail.com>
2024-01-27 23:06:12 +0200

    new thing

--- script.js
+++ script.js
@@ -5,23 +5,17 @@ function sendBums(choice) {
       alert("Good Shot, I am online on Whatsapp NOW!");
     }
   }
-  
-  let noButtonClickable = true;
-  
+
   function runAway() {
-    if (noButtonClickable) {
-      moveNoButton();
-      noButtonClickable = false;
-    }
+    moveNoButton();
   }
-  
+
   function moveNoButton() {
     const button = document.getElementById("no-button");
     const newPositionX = Math.random() * (window.innerWidth - button.offsetWidth);
     const newPositionY = Math.random() * (window.innerHeight - button.offsetHeight);
-  
+
     button.style.position = "absolute";
     button.style.left = newPositionX + "px";
     button.style.top = newPositionY + "px";
   }
-  
