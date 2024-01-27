ac9b91b3c77d5b12bd4ae13625003e304a4b8d58
Alfred Zimo <zimoalfred@gmail.com>
2024-01-27 22:51:44 +0200

    NEW COMMIT

--- script.js
+++ script.js
@@ -1,44 +1,27 @@
 // script.js
 
-const urlParams = new URLSearchParams(window.location.search);
-const username = urlParams.get('username');
-
-if (username) {
-  document.getElementById('username').innerText = username;
-}
-
-function startGame() {
-  const username = document.getElementById('username').value;
-
-  if (username) {
-    // Open the new game page
-    window.location.href = `game.html?username=${encodeURIComponent(username)}`;
-  } else {
-    alert('Please enter your name.');
-  }
-}
-
 function sendBums(choice) {
-  if (choice === 'yes') {
-    alert("Good Shot, I am online on Whatsapp!");
+    if (choice === 'yes') {
+      alert("Good Shot, I am online on Whatsapp NOW!");
+    }
   }
-}
-
-let noButtonClickable = true;
-
-function runAway() {
-  if (noButtonClickable) {
-    moveNoButton();
-    noButtonClickable = false;
+  
+  let noButtonClickable = true;
+  
+  function runAway() {
+    if (noButtonClickable) {
+      moveNoButton();
+      noButtonClickable = false;
+    }
   }
-}
-
-function moveNoButton() {
-  const button = document.getElementById("no-button");
-  const newPositionX = Math.random() * (window.innerWidth - button.offsetWidth);
-  const newPositionY = Math.random() * (window.innerHeight - button.offsetHeight);
-
-  button.style.position = "absolute";
-  button.style.left = newPositionX + "px";
-  button.style.top = newPositionY + "px";
-}
+  
+  function moveNoButton() {
+    const button = document.getElementById("no-button");
+    const newPositionX = Math.random() * (window.innerWidth - button.offsetWidth);
+    const newPositionY = Math.random() * (window.innerHeight - button.offsetHeight);
+  
+    button.style.position = "absolute";
+    button.style.left = newPositionX + "px";
+    button.style.top = newPositionY + "px";
+  }
+  
